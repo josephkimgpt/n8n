01155622ac5616e0f688f431c3f77f4c6bf96b33
josephkimgpt <josephkimgpt@users.noreply.github.com>
2025-08-04 02:38:13 +0000

    https in editor URL

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-export N8N_EDITOR_BASE_URL=n8n.groundupcoder.com
+export N8N_EDITOR_BASE_URL="https://n8n.groundupcoder.com"
 export N8N_RUNNERS_ENABLED=true
 
 npx n8n
